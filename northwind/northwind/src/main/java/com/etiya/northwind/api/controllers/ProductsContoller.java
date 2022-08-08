6411438359e50172d9d8004c76b9565e791231a6
tugaysert <mustafatugaysert@gmail.com>
2022-08-08 11:56:25 +0300

    -

---
+++ northwind/northwind/src/main/java/com/etiya/northwind/api/controllers/ProductsContoller.java
@@ -0,0 +1,26 @@
+package com.etiya.northwind.api.controllers;
+
+import com.etiya.northwind.business.abstracts.ProductService;
+import com.etiya.northwind.business.responses.products.ProductListResponse;
+import com.etiya.northwind.entities.concretes.Product;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+
+public class ProductsContoller {
+    private final ProductService productService;
+
+    public ProductsContoller(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/getall")
+    public List<ProductListResponse> getAll() {
+        return this.productService.getAlL();
+    }
+}

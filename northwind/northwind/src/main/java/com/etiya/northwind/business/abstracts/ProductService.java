138cc89d3922693492301e6f29e846f3af5b991f
tugaysert <mustafatugaysert@gmail.com>
2022-08-08 19:28:52 +0300

    -

--- northwind/northwind/src/main/java/com/etiya/northwind/business/abstracts/ProductService.java
+++
@@ -1,8 +0,0 @@
-package com.etiya.northwind.business.abstracts;
-
-import com.etiya.northwind.business.responses.products.ProductListResponse;
-import java.util.List;
-
-public interface ProductService {
-    List<ProductListResponse> getAlL();
-}
--- northwind/northwind/src/main/java/com/etiya/northwind/dataAccess/abstracts/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.etiya.northwind.dataAccess.abstracts;
-
-import com.etiya.northwind.entities.concretes.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-
-}
